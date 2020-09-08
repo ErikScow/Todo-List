05030dd18ed4aff55776b756a8fc90a91e868c5c
erikscow <erikscow@gmail.com>
2020-09-08 00:56:59 -0700

    added validation middleware functions for crud on users

--- back-end/users/users-middleware.js
+++ back-end/users/users-middleware.js
@@ -1,9 +1,12 @@
+const db = require('./users-model')
 const jwt = require('jsonwebtoken')
 const secrets = require('../config/secrets')
-const { orWhereNotExists } = require('../data/db-config')
 
 module.exports = {
     authenticate,
+    isUnique,
+    validateUser,
+    validateId
 }
 
 function authenticate(req, res, next){
@@ -22,6 +25,40 @@ function authenticate(req, res, next){
     } else {
         res.status(401).json({ message: 'no token included' })
     }
+}
 
-    
+async function isUnique(req, res, next){
+    const username = req.body.username
+    const usernameTaken = await db.findByUsername(username)
+
+    if (usernameTaken){
+        res.status(400).json({ message: "username taken"})
+    } else {
+        next()
+    }
+}
+
+async function validateUser(req, res, next) {
+    const { username, password } = req.body
+    if (username, password){
+        next()
+    } else {
+        res.status(400).json({ message: 'must include username and password' })
+    }
+}
+
+async function validateId(req, res, next){
+    const id  = req.params.id
+        
+    try{
+        const exists = await db.findById(id)
+        if (exists){
+            next()
+        } else {
+            res.status(400).json({ message: 'invalid user id'})
+        }
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({ message: 'could not validate user' })
+    }
 }
