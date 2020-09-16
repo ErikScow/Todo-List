3b444227789493d96f84f08735364014ee048c46
ErikScow <erikscow@gmail.com>
2020-09-16 12:30:56 -0700

    added helmet and cors middleware to server

--- back-end/server.js
+++ back-end/server.js
@@ -1,10 +1,15 @@
 const express = require('express')
+const cors = require('cors')
+const helmet = require('helmet')
 
 require('dotenv').config()
+
 const server = express()
 const userRouter = require('./users/users-router')
 
 server.use(express.json())
+server.use(cors())
+server.use(helmet())
 server.use('/api/users', userRouter)
 
 const port = process.env.PORT
