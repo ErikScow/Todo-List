bfff11d639807e058daefb58358075d617fe9019
ErikScow <erikscow@gmail.com>
2020-09-30 09:30:57 -0700

    removed console logs

--- front-end/src/Components/PrivateRoute.js
+++ front-end/src/Components/PrivateRoute.js
@@ -19,7 +19,6 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
     useEffect(() => {
         if (!userData.id){
             const data = localStorage.getItem('user-state')
-            console.log(JSON.parse(data))
             setUserData(JSON.parse(data))
             setStateChecked(true)
         } else {
@@ -33,17 +32,13 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
         //only runs this code after we have verified state is present and or reset it from localstorage. this is here to prevent the component from unmounting before the token is verified
         if (stateChecked){
             const token = localStorage.getItem('token')
-            console.log('token '  , token)
-            console.log('id ',userData)
             if (token && userData.id){
             axiosWithAuth().get(`http://localhost:5000/api/users/${userData.id}`)
                 .then(() => {
-                    console.log('authenticated')
                     setAuthenticated(true)
                     setLoggedIn(true)
                 })
                 .catch(() => {
-                    console.log('rejected')
                     setAuthenticated(false)
                     localStorage.removeItem('token')
                 })
@@ -51,15 +46,11 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
                     setCompleted(true)
                 })
             } else {
-                console.log('missing id or token')
                 setCompleted(true)
             }
         }
     },[stateChecked])
     
-    
-    console.log('auth', authenticated)
-    console.log('completed', completed)
     if (!completed){
         return <div>Loading</div>
     }    
@@ -69,10 +60,8 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
         {...rest}
         render={props => {
             if(!authenticated){
-                console.log('will not render')
                 return <Redirect to="/login"/>
             } else {
-                console.log('will render ',authenticated)
                 return <Component {...props}/>
                 
             }
