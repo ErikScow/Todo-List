45852c0ab0907058551fb885b59cd2005dfcbe40
Erik Scow <erikscow@gmail.com>
2020-10-10 13:01:49 -0700

    made task names the same across all levels of subtasks for a dryer front end code.

--- back-end/data/seeds/03-sub_tasks.js
+++ back-end/data/seeds/03-sub_tasks.js
@@ -3,8 +3,8 @@ exports.seed = function(knex) {
   return knex('sub_tasks').insert([
     {
       task_id: 1,
-      sub_task_name: 'some sub task',
-      sub_task_description: 'the description of that sub task',
+      task_name: 'some sub task',
+      task_description: 'the description of that sub task',
       created: '09-13-2020'
     }
   ]);
--- back-end/data/seeds/04-sub_tasks_2.js
+++ back-end/data/seeds/04-sub_tasks_2.js
@@ -3,8 +3,8 @@ exports.seed = function(knex) {
   return knex('sub_tasks_2').insert([
     {
       sub_task_id: 1,
-      sub_task_name: 'some sub sub task',
-      sub_task_description: 'the description of that sub sub task',
+      task_name: 'some sub sub task',
+      task_description: 'the description of that sub sub task',
       created: '09-13-2020'
     }
   ]);
