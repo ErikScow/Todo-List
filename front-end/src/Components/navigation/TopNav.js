2211161a3ad9b43a332b572f7474f294eda59bc6
ErikScow <erikscow@gmail.com>
2020-11-12 08:14:47 -0800

    started component for secondary nav bar

---
+++ front-end/src/Components/navigation/TopNav.js
@@ -0,0 +1,12 @@
+import React from React
+
+const TopNav = () => {
+    return(
+        <div>
+            <h1>My Lists</h1>
+            <button>Gear Icon</button>
+        </div>
+    )
+}
+
+export default TopNav
