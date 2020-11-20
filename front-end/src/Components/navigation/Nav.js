7d33f67c09640992b03c6bb4c5a610e24adb552c
ErikScow <erikscow@gmail.com>
2020-11-19 18:10:48 -0800

    Styling done for the most part

--- front-end/src/Components/navigation/Nav.js
+++ front-end/src/Components/navigation/Nav.js
@@ -13,38 +13,56 @@ const Nav = () => {
     const [loggedInStatus, setLoggedIn] = loggedIn
     const [userData, setUserData] = user
 
+    const [dropdown, setDropdown] = useState('hidden')
+
     const location = useLocation()
     let activeTab = ''
     let completedTab = ''
     let discardedTab = ''
 
 
-        if (location.pathname === '/active'){
-            activeTab = 'current'
-            completedTab = ''
-            discardedTab = ''
-        } else if (location.pathname === '/completed'){
-            activeTab = ''
-            completedTab = 'current'
-            discardedTab = ''
-        } else if (location.pathname === '/discarded'){
-            activeTab = ''
-            completedTab = ''
-            discardedTab = 'current'
+    if (location.pathname === '/active'){
+        activeTab = 'current'
+        completedTab = ''
+        discardedTab = ''
+    } else if (location.pathname === '/completed'){
+        activeTab = ''
+        completedTab = 'current'
+        discardedTab = ''
+    } else if (location.pathname === '/discarded'){
+        activeTab = ''
+        completedTab = ''
+        discardedTab = 'current'
+    }
+
+    const toggleDropdown = () => {
+        if (dropdown === 'hidden'){
+            setDropdown('')
+        } else {
+            setDropdown('hidden')
         }
-    
+    }
+
     const activeClass = `tabs-link ${activeTab}`
     const completedClass = `tabs-link ${completedTab}`
     const discardedClass = `tabs-link ${discardedTab}`
 
+    const linkContainerClass = `links-container ${dropdown}`
+    const topNavClass = `top-nav ${dropdown}`
+
+    const firstInDropDownClass = `first-in-drop ${dropdown}`
+    
     if (loggedInStatus === false){
         return(
-            <div className='top-nav'>
-                <h1>My Lists</h1>
-                <div className='links-container'>
-                    <Link  to="/"><button>Home</button></Link>
-                    <Link  to="/login"><button>Login</button></Link>
-                    <Link  to="/register"><button>Sign Up</button></Link>
+            <div className={topNavClass}>
+                <div className = 'wrapper'>
+                    <h1>My Lists</h1>
+                    <a className = 'icon ' href='javascript:void(0);' onClick={toggleDropdown}>☰</a>
+                </div>
+                <div className={linkContainerClass}>
+                    <Link className={firstInDropDownClass} to="/" onClick={toggleDropdown}><button>About</button></Link>
+                    <Link className={dropdown} to="/login" onClick={toggleDropdown}><button>Login</button></Link>
+                    <Link className={dropdown} to="/register" onClick={toggleDropdown}><button>Sign Up</button></Link>
                 </div>
                 
             </div>
